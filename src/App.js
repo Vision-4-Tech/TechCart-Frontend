894dc9df812a3c800038b669013c502bf7a2b619
Suhasp987 <ps9781094@gmail.com>
2024-06-28 13:46:00 +0530

    suahs

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 
 import Admin from './Admin/Admin';
 import Signup from './components/Signup';
-import {BrowserRouter,Routes,Route,Router} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Signin from './components/Signin';
 import Home from './components/Home';
 import Details from './Admin/Details';
 function App() {
   return (
     <div className="App">
+    <h1>Welcme to Tech cart</h1>
        <Router>
        <Routes>
           <Route path='/' element={<Signin/>} />
           <Route path='/login' element={<Signup/>} />
-          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
           <Route path='/:text' element={<Home/>} />
           <Route path='/Admin' element={<Admin/>}></Route>
           <Route path='/Admin/:text' element={<Admin/>}></Route>
