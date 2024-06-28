8ce52d2f2a5ef2fbb0d240040717eadd75d3fed5
Suhasp987 <ps9781094@gmail.com>
2024-06-28 10:07:15 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 import Admin from './Admin/Admin';
 import Signup from './components/Signup';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Router} from 'react-router-dom'
 import Signin from './components/Signin';
 import Home from './components/Home';
 import Details from './Admin/Details';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+       <Router>
        <Routes>
           <Route path='/' element={<Signin/>} />
           <Route path='/login' element={<Signup/>} />
@@ -20,7 +20,8 @@ function App() {
           <Route path='/Admin/:text' element={<Admin/>}></Route>
           <Route path='/details/:id' element={<Details/>}></Route>
        </Routes>
-    </BrowserRouter>
+       </Router>
+    
 
     
     </div>
